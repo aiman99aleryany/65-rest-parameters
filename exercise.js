2e0dfdc1a5c45ed694d33c4005d2e3b1e7366022
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-10-12 23:43:04 +0300

    init

---
+++ exercise.js
@@ -0,0 +1,13 @@
+// The sum function has a high numbers of parameters. How can we improve the code in order to make it accept an unknown numbers to sum?
+
+// function sum(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+function sum(...nums) {
+  return nums.reduce((pre, el) => pre + el, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5));
